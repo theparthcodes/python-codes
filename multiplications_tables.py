1b5e874190c0bcaefa3a6ab1f7faef59f42ae94b
Parth Ugale <parthugale23@gmail.com>
2025-06-22 12:56:32 +0530

    added multiplication table project using for loop 📊

---
+++ multiplications_tables.py
@@ -0,0 +1,11 @@
+# My first loop-based project 🚀
+# This prints the multiplication table of any number 📊
+
+n = int(input("Enter a number :")) # input from the user 
+
+for i in range(1,11):
+    print((f"{n} X {i} = {n*i}"))
+    
+else:
+    print("Done.....")
+    
